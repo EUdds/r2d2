059c9f8eb9028028c95c188f2647536d356572df
Eric Udlis <udlis.eric@gmail.com>
2025-12-16 17:20:49 -0800

    v1 work

--- holoprojector/firmware/v1/include/status_led.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "stdint.h"
-
-
-void status_led_init(void);
-void status_led_toggle(void);
-void fatal_blink(uint32_t period);
--- holoprojector/firmware/v1/include/tasks.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-void task_setup(void);
-void task_1hz(void);
-void task_10hz(void);
-void task_100hz(void);
-void task_1khz(void);
